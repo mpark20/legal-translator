fc7b8a006c9adec738d49085828640f6377ec087
mpark20 <mollysypark@gmail.com>
2023-03-27 09:59:46 -0700

    char limit and bullet points

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { Configuration, OpenAIApi } from "openai";
 const Home = () => {
   const [file, setFile] = useState("");
   const [legalData, setlegalData] = useState("");
+  const [charCount, setCharCount] = useState(0);
   const [explanation, setExplanation] = useState([]);
   const [summary, setSummary] = useState([]);
   const [result, setResult] = useState("");
@@ -15,7 +16,7 @@ const Home = () => {
   const openai = new OpenAIApi(configuration);
 
   useEffect(() => {
-    console.log(legalData);
+    //console.log(legalData);
     console.log(result);
     console.log(explanation);
     console.log(file);
@@ -27,15 +28,20 @@ const Home = () => {
   const uploadFile = () => {
     
   }
-
+  const handleChange = (e) => {
+    // update character count
+    setCharCount(e.target.value.length);
+    // update data
+    setlegalData(e.target.value);
+  }
   const handleSubmit = async (e) => {
 
     e.preventDefault();
     // show "loading..." text
     const explanationElement = document.getElementById("loading");
     explanationElement.classList.remove("hidden");
-
-    // Update medical data loading state with true
+    
+    // Update legal data loading state with true
     setlegalDataLoading(true);
 
     // Implement check to make sure it is not a blank submission
@@ -44,7 +50,7 @@ const Home = () => {
       let response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt:
-          "Define the legal terms in the following text on separate lines in plain english:\n\n " + legalData,
+          "Define the legal terms in the following text on separate lines in plain english:\n " + legalData.replace(/^\s*$(?:\r\n?|\n)/gm, ""),
         temperature: 0.7,
         max_tokens: 256,
         top_p: 1,
@@ -55,7 +61,7 @@ const Home = () => {
       
 
       // Update explanation state with the response from GPT-3
-      var res = response.data.choices[0].text;
+      var res = response.data.choices[0].text.replace(/^\s*$(?:\r\n?|\n)/gm, "");
       setResult(res);
       // separate each term's description
       var lines = res.split(/\r?\n/);
@@ -63,7 +69,7 @@ const Home = () => {
       let response2 = await openai.createCompletion({
         model: "text-davinci-003",
         prompt:
-          "Summarize the most important points from the following text in a way a 15 year old could understand with each point on a new line beginning with an asterisk:\n\n " + legalData,
+          "Summarize the most important points from the following text in a way a 15 year old could understand with each point on a new line beginning with an asterisk:\n " + legalData,
         temperature: 0.7,
         max_tokens: 256,
         top_p: 1,
@@ -72,7 +78,7 @@ const Home = () => {
       });
 
       // Update summary state with the response from GPT-3
-      var res2 = response2.data.choices[0].text;
+      var res2 = response2.data.choices[0].text.replace(/^\s*$(?:\r\n?|\n)/gm, "");
       console.log(res2);
       var res2Final = res2.split("*")
       var res2Trimmed = res2Final.filter(item => item.length > 1)
@@ -92,7 +98,7 @@ const Home = () => {
       }
       
 
-      // Update medical data loading state with false to allow explanation to conditionally render
+      // Update legal data loading state with false to allow explanation to conditionally render
       setlegalDataLoading(false);
     } else {
       explanationElement.innerText = "You did not submit anything, please submit again."
@@ -108,20 +114,18 @@ const Home = () => {
         <label className="block text-gray-700 mb-2 heading" for="legal_data">
           enter text:
         </label>
-        {/*<input
-          class="p-3 mb-4 rounded-lg w-1/2 md:w-1/3 lg:w-1/4 sandy"
-          type="text"
-          id="legal_data"
-          onChange={(e) => setlegalData(e.target.value)}
-  />*/}
+        
         <textarea 
-          className="p-3 mb-4 rounded-lg w-1/2 md:w-1/3 lg:w-1/4 sandy" 
+          className="p-3 mb-2 rounded-lg w-3/4 sandy" 
           id="legal_data"
-          onChange={(e) => setlegalData(e.target.value)}
+          onChange={handleChange}
           name="legalData" 
           cols="40" 
-          rows="5">
+          rows="5"
+          maxLength="5000"
+          >
         </textarea>
+        <p className="mb-4 ">{charCount}/5000 characters</p>
         <button
           className="submit-btn hover:bg-gray-700 mb-4 font-medium py-2 px-4 rounded-lg min-w-fit w-1/6 md:w-1/8 lg:w-1/12"
           type="submit"
@@ -133,19 +137,19 @@ const Home = () => {
       {/* we set explanation element to be loading... or the explanation text based on conditional rendering */}
       {legalDataLoading == false && (
         <div className="report">
-          <div class="report-col">
+          <div className="report-col">
             <label className="block text-gray-700 font-medium" for="explanation">
               Definitions:
             </label>
             <div >
-              <p id="explanation" className="p">
+              <div id="explanation" className="p">
                 {explanation.map((line, index) => (
                   <div key={index} className="desc">
                     <b>{line.substring(0, line.indexOf(":") + 1)}</b>
                     <p>{line.substring(line.indexOf(":") + 1)}</p>
                   </div>
                 ))}
-              </p>
+              </div>
             </div>
           </div>
           <div className="report-col">
